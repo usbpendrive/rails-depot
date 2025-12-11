15baec650359e805c82e6d7f5d9012fd4fea02ae
usbpendrive <ferry.fernando@gmail.com>
2025-12-11 08:49:40 +0700

    Scaffold Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_11_000507) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_11_014549) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
